384bea3eb9a132926ca11781cc31f2faae087005
mkluonius <mindaugas.kluonius@gmail.com>
2017-04-12 20:18:36 +0300

    Pirma uzduotis

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,16 +3,21 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 
 public class Controller { // veiksmus rasysime kuriuos programuosim ant mygtuku
 
     @FXML
     private Label text;
+    @FXML
+    private TextField textField;
+    @FXML
+    private TextField textField1;
 
     public void sayHi(ActionEvent event){
-        String prefix = text.getText();
-        text.setText(prefix + " , Jau Isvyk!");
+        text.setText(textField.getText() + " " + textField1.getText());
+
 
 
     }
